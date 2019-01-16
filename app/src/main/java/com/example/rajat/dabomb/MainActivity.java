305faf477732319a62a68d09171185291f58aba0
therajat08 <therajat08@users.noreply.github.com>
2019-01-16 20:45:15 +0530

    at this point we can use go button and timer goes to 0 sec

--- app/src/main/java/com/example/rajat/dabomb/MainActivity.java
+++ app/src/main/java/com/example/rajat/dabomb/MainActivity.java
@@ -3,20 +3,53 @@ package com.example.rajat.dabomb;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    public void updateTimer(int secondsLeft){
+        int minutes = (int)secondsLeft/60;//casting done for roundin off the num
+        int seconds = secondsLeft - minutes * 60;
 
+        String secondString = Integer.toString(seconds);
+          if(seconds <= 9)
+        {
+            secondString = "0" + secondString;
+        }
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+
+    }
+    public void controlTimer(View view){
+        new CountDownTimer(timerSeekBar.getProgress()*1000 + 100,1000){
+            //mul by 1000 cos seconds to milli sec
+            //addded 100 cos it takes some millisc for onTick to be called and we are roundin off in
+            //updatatimer so it timer wan't workin right    
+            @Override
+            public void onTick(long millisUNtilFinished) {
+                updateTimer((int) millisUNtilFinished/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                timerTextView.setText("0:00");//cos it was like: 0:1
+                Log.i("finished","coundown");
+            }
+
+
+        }.start();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
-        final TextView timerTextView = (TextView)findViewById(R.id.timerTextView);
+        timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+        timerTextView = (TextView)findViewById(R.id.timerTextView);
         timerTextView.setEnabled(false);
         timerSeekBar.setMax(600);//10 * 60 for 10 minumtes
         timerSeekBar.setProgress(30);
@@ -24,17 +57,7 @@ public class MainActivity extends AppCompatActivity {
         timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                int minutes = (int)progress/60;//casting done for roundin off the num
-                int seconds = progress - minutes * 60;
-
-                String secondString = Integer.toString(seconds);
-
-                if(secondString == "0")//this is done cos at the end it was like 10:0 and not 10:00
-                {
-                    secondString = "00";
-                }
-                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
-
+                updateTimer(progress);
             }
 
             @Override
